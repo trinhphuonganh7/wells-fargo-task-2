806540c898bb49004078e307869788c8af270da3
trinhphuonganh7 <trinhphganh775@gmail.com>
2026-06-13 22:19:50 -0700

    Create Client.java

---
+++ src/main/java/com/wellsfargo/counselor/entities/Client.java
@@ -0,0 +1,81 @@
+package com.wellsfargo.counselor.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long clientId;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false)
+    private String email;
+
+    @ManyToOne
+    @JoinColumn(name = "advisor_id", nullable = false)
+    private Advisor advisor;
+
+    @OneToOne(mappedBy = "client", cascade = CascadeType.ALL)
+    private Portfolio portfolio;
+
+    protected Client() {}
+
+    public Client(String firstName, String lastName, String email, Advisor advisor, Portfolio portfolio) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.advisor = advisor;
+        this.portfolio = portfolio;
+    }
+
+    public Long getClientId() {
+        return clientId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Advisor getAdvisor() {
+        return advisor;
+    }
+
+    public void setAdvisor(Advisor advisor) {
+        this.advisor = advisor;
+    }
+
+    public Portfolio getPortfolio() {
+        return portfolio;
+    }
+
+    public void setPortfolio(Portfolio portfolio) {
+        this.portfolio = portfolio;
+    }
+}
