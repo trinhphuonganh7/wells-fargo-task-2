11078518e2a78a12e6de828b44ba1c97ade8a042
trinhphuonganh7 <trinhphganh775@gmail.com>
2026-06-13 22:20:30 -0700

    Create Portfolio entity with client and securities

---
+++ src/main/java/com/wellsfargo/counselor/entities/Portfolio.java
@@ -0,0 +1,46 @@
+package com.wellsfargo.counselor.entities;
+
+import jakarta.persistence.*;
+import java.util.List;
+
+@Entity
+public class Portfolio {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long portfolioId;
+
+    @OneToOne
+    @JoinColumn(name = "client_id", nullable = false, unique = true)
+    private Client client;
+
+    @OneToMany(mappedBy = "portfolio", cascade = CascadeType.ALL)
+    private List<Security> securities;
+
+    protected Portfolio() {}
+
+    public Portfolio(Client client, List<Security> securities) {
+        this.client = client;
+        this.securities = securities;
+    }
+
+    public Long getPortfolioId() {
+        return portfolioId;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+    public List<Security> getSecurities() {
+        return securities;
+    }
+
+    public void setSecurities(List<Security> securities) {
+        this.securities = securities;
+    }
+}
